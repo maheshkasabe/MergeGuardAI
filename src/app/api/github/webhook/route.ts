078d760323913629d48ac7da919b9f1ca637de3b
Mahesh Kasbe <maheshkasbe010@gmail.com>
2024-11-06 13:56:26 +0530

    test commit

--- src/app/api/github/webhook/route.ts
+++ src/app/api/github/webhook/route.ts
@@ -2,6 +2,11 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest){
     const body = await req.json();
-    console.log(body);
+    const action = body.action;
+    const prNumber = body.pull_request?.number;
+
+    console.log("Action:", action);
+    console.log("Pull Request Number:", prNumber);
+
     return NextResponse.json({ message: 'Webhook received' });
 }
