e83c745aa5e4c05d33817ba345be2f9b7d404d7f
Mahesh Kasbe <maheshkasbe010@gmail.com>
2024-11-06 16:07:29 +0530

    test commit

--- src/app/api/github/webhook/route.ts
+++ src/app/api/github/webhook/route.ts
@@ -1,12 +1,41 @@
+import { Octokit } from '@octokit/rest';
+import axios from 'axios';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextRequest){
-    const body = await req.json();
-    const action = body.action;
-    const prNumber = body.pull_request?.number;
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN});
+const [owner, repo] = (process.env.REPO_NAME as string).split("/");
 
-    console.log("Action:", action);
-    console.log("Pull Request Number:", prNumber);
+export async function POST(req: NextRequest){
+    const { action, pull_request } = await req.json();
+
+    if (pull_request && (action === 'opened' || action === 'synchronize')){
+        const prNumber = pull_request.number;
+        console.log(`Checking PR #${prNumber}: ${pull_request.title}`);
+        await getPrFiles(prNumber);
+        await getFileContent(prNumber);
+    }
 
     return NextResponse.json({ message: 'Webhook received' });
+}
+
+async function getPrFiles(prNumber: number){
+    const {data: files } = await octokit.rest.pulls.listFiles({
+        owner,
+        repo,
+        pull_number: prNumber,
+    })
+    console.log(files);
+    return files;
+}
+
+async function getFileContent(prNumber: number){
+    const files = await getPrFiles(prNumber);
+
+    for (const file of files){
+        const {raw_url}= file;
+        const {data: fileContent} = await axios.get(raw_url);
+        console.log(fileContent);
+        return fileContent
+    }
+
 }
